bc8b6e5a2d21f4a74344d383022906b63a5abe11
Bartek Bugała <bartek.bugala@gmail.com>
2019-10-14 22:21:27 +0200

    init server new file:   server/.gitignore new file:   server/package.json new file:   server/server.js new file:   server/yarn.lock

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/api/posts', (req, res) => {
+  const data = [
+    {
+      id: 'abcdefg1',
+      title: 'Lorem Ipsum',
+      content: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.'
+    },
+    {
+      id: 'abcdefg2',
+      title: 'Ipsum Lorem',
+      content: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.'
+    }
+  ];
+  res.json(data);
+});
+
+app.listen(8000, function() {
+  console.log('Server is running on port:', 8000);
+});
