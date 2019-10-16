2557bac1e1d2001971ab2ebf738b2aab0e656b7c
Bartek Bugała <bartek.bugala@gmail.com>
2019-10-17 00:51:01 +0200

    add PostsList

--- client/src/components/features/Posts/Posts.js
+++ client/src/components/features/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import PostsList from '../PostsList/PostsList';
 
 class Posts extends React.Component {
   componentDidMount() {
@@ -11,12 +12,7 @@ class Posts extends React.Component {
 
     return (
       <div>
-        Posts
-        <ul>
-          {posts.map(post => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
+        <PostsList posts={posts} />
       </div>
     );
   }
