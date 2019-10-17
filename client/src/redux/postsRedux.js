2f457a46e37f1c7a984be8e7e5c7945127b225b5
Bartek Bugała <bartek.bugala@gmail.com>
2019-10-17 20:30:20 +0200

    edit postsRedux.js

--- client/src/redux/postsRedux.js
+++ client/src/redux/postsRedux.js
@@ -2,23 +2,34 @@ import axios from 'axios';
 import { API_URL } from '../config';
 
 //// Selectors
-export const getPosts = ({ posts }) => posts;
-export const countPosts = ({ posts }) => posts.length;
+export const getPosts = ({ posts }) => posts.data;
+export const countPosts = ({ posts }) => posts.data.length;
+export const getRequest = ({ posts }) => posts.request;
 
 //// Thunks
 export const loadPostsRequest = () => {
   return async dispatch => {
+    dispatch(startRequest());
     try {
       let res = await axios.get(`${API_URL}/posts`);
+      await new Promise((resolve, reject) => setTimeout(resolve, 2000));
       dispatch(loadPosts(res.data));
+      dispatch(endRequest());
     } catch (e) {
-      console.log(e.message);
+      dispatch(endRequest(e.message));
     }
   };
 };
 
 //// Initial state
-const initialState = [];
+const initialState = {
+  data: [],
+  request: {
+    pending: false,
+    error: null,
+    success: null
+  }
+};
 
 //// Actions
 // action name creator
@@ -27,13 +38,24 @@ const createActionName = name => `app/${reducerName}/${name}`;
 
 // action exports
 export const LOAD_POSTS = createActionName('LOAD_POSTS');
+export const START_REQUEST = createActionName('START_REQUEST');
+export const END_REQUEST = createActionName('END_REQUEST');
+export const ERROR_REQUEST = createActionName('ERROR_REQUEST');
+
 export const loadPosts = payload => ({ payload, type: LOAD_POSTS });
+export const startRequest = () => ({ type: START_REQUEST });
+export const endRequest = () => ({ type: END_REQUEST });
+export const errorRequest = error => ({ error, type: ERROR_REQUEST });
 
 //// Reducer
 export default function reducer(statePart = initialState, action = {}) {
   switch (action.type) {
     case LOAD_POSTS:
-      return [...action.payload];
+      return { ...statePart, data: action.payload };
+    case START_REQUEST:
+      return { ...statePart, request: { pending: true, error: null, success: null } };
+    case END_REQUEST:
+      return { ...statePart, request: { pending: false, error: null, success: true } };
     default:
       return statePart;
   }
