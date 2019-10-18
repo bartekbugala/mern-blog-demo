ba96bc0cb07d703822a18eb0ad01c32a7130b0c9
Bartek Bugała <bartek.bugala@gmail.com>
2019-10-19 01:44:05 +0200

    add mongoose, nodemon

--- server/server.js
+++ server/server.js
@@ -11,12 +11,7 @@ const postRoutes = require('./routes/post.routes');
 app.use(cors());
 app.use(express.urlencoded({ extended: true })); // false?
 app.use(express.json());
-
 app.use('/api', postRoutes);
-
-app.listen(config.PORT, function() {
-  console.log('Server is running on port:', config.PORT);
-});
 
 // mongoDB - connect backend code with db
 mongoose.connect(config.MONGO_URL, { useNewUrlParser: true });
@@ -24,3 +19,7 @@ let db = mongoose.connection;
 
 db.once('open', () => console.log('Connected to database'));
 db.on('error', err => console.log('Error ' + err));
+
+app.listen(config.PORT, function() {
+  console.log('Server is running on port:', config.PORT);
+});
