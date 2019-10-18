03fdb2a172dd302f0dfde61dbe3eb7b0fe995991
Bartek Bugała <bartek.bugala@gmail.com>
2019-10-19 01:55:53 +0200

    pre-task updates add test data, helmet controllers, model, server
    modified:   controllers/post.controller.js
    new file:   models/post.model.js
    modified:   package.json
    modified:   server.js
    new file:   testData.js
    modified:   yarn.lock

--- server/server.js
+++ server/server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const config = require('./config');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+
+const loadTestData = require('./testData');
 
 const app = express();
 
 // import routes
 const postRoutes = require('./routes/post.routes');
 
+app.use(helmet());
 app.use(cors());
 app.use(express.urlencoded({ extended: true })); // false?
 app.use(express.json());
@@ -17,7 +21,10 @@ app.use('/api', postRoutes);
 mongoose.connect(config.MONGO_URL, { useNewUrlParser: true });
 let db = mongoose.connection;
 
-db.once('open', () => console.log('Connected to database'));
+db.once('open', () => {
+  console.log('Connected to the database');
+  loadTestData();
+});
 db.on('error', err => console.log('Error ' + err));
 
 app.listen(config.PORT, function() {
