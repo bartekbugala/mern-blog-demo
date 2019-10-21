e63f8f74be94d9f4f7f4e0ab157df6e05f5c1614
Bartek Bugała <bartek.bugala@gmail.com>
2019-10-21 02:47:30 +0200

    task 31-2 modified:   src/components/common/HtmlBox/HtmlBox.js modified:   src/components/features/PostForm/PostForm.js modified:   src/redux/postsRedux.js Add empty string prevention

--- client/src/components/features/PostForm/PostForm.js
+++ client/src/components/features/PostForm/PostForm.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
-//import { connect } from 'react-redux';
 
 import TextField from '../../common/TextField/TextField';
 import SectionTitle from '../../common/SectionTitle/SectionTitle';
@@ -15,6 +14,7 @@ import './PostForm.scss';
 
 class PostForm extends React.Component {
   state = {
+    request: {},
     post: {
       title: '',
       author: '',
@@ -27,35 +27,54 @@ class PostForm extends React.Component {
   };
   handleEditor = text => {
     const { post } = this.state;
-    this.setState({ post: { ...post, content: text } });
+    this.setState({ post: { ...post, content: text.trim() } });
   };
   addPost = e => {
     const { addPost } = this.props;
     const { post } = this.state;
-
+    const { request } = this.props;
     e.preventDefault();
+    if (post.content.trim().length === 0 || post.content === '<p><br></p>') {
+      return;
+    }
+    this.setState({ request: request });
     addPost(post);
   };
 
   render() {
     const { post } = this.state;
     const { handleChange, handleEditor, addPost } = this;
-    const { request } = this.props;
+    const { request } = this.state;
 
     if (request.error) return <Alert variant="error">{request.error}</Alert>;
-    else if (request.success) return <Alert variant="success">Post has been added!</Alert>;
+    else if (request.success && post.content !== '') return <Alert variant="success">Post has been added!</Alert>;
     else if (request.pending) return <Spinner />;
     else
       return (
         <form onSubmit={addPost}>
-          <TextField label="Title" value={post.title} onChange={handleChange} name="title" />
-          <TextField label="Author" value={post.author} onChange={handleChange} name="author" />
+          <TextField label="Title" value={post.title} onChange={handleChange} name="title" required />
+          <TextField label="Author" value={post.author} onChange={handleChange} name="author" required />
           <SectionTitle>Edit post content</SectionTitle>
           <Editor
             className="content-editor"
             text={post.content}
             onChange={handleEditor}
-            options={{ placeholder: true, toolbar: { buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3'] } }}
+            options={{
+              placeholder: true,
+              toolbar: { buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3'] },
+              disableExtraSpaces: true,
+              disableDoubleReturn: true,
+              paste: {
+                /* This example includes the default options for paste,
+                   if nothing is passed this is what it used */
+                forcePlainText: false,
+                cleanPastedHTML: true,
+                cleanReplacements: [],
+                cleanAttrs: ['class', 'style', 'dir'],
+                cleanTags: ['meta'],
+                unwrapTags: []
+              }
+            }}
           />
 
           <Button variant="primary">Add post</Button>
--- client/src/redux/postsRedux.js
+++ client/src/redux/postsRedux.js
@@ -42,8 +42,8 @@ export const addPostRequest = post => {
   return async dispatch => {
     dispatch(startRequest());
     try {
-      let res = await axios.post(`${API_URL}/posts`, post);
-      await new Promise((resolve, reject) => setTimeout(resolve, 2000)); ////////////////////////// REMOVE ME
+      await axios.post(`${API_URL}/posts`, post);
+      await new Promise((resolve, reject) => setTimeout(resolve, 1000)); // REMOVE ME
       dispatch(endRequest());
     } catch (e) {
       dispatch(errorRequest(e.message));
