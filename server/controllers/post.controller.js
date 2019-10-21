2ebc07104c583e99e435326afa45f2400ea1756a
Bartek Bugała <bartek.bugala@gmail.com>
2019-10-21 02:49:27 +0200

    modified:   server/controllers/post.controller.js

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -25,12 +25,11 @@ exports.addPost = async (req, res) => {
   try {
     const { title, author, content } = req.body;
 
-    //    let newPost = new Post();
-    //    newPost.title = title;
-    //    newPost.author = author;
-    //    newPost.content = content;
-
-    let newPost = new Post(req.body);
+    let newPost = new Post();
+    newPost.title = title;
+    newPost.author = author;
+    newPost.content = content;
+    //let newPost = new Post(req.body);
     newPost.id = uuid();
 
     postSaved = await newPost.save();
