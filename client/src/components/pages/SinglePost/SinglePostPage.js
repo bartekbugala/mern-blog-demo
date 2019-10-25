e8f8fb6968339dd1891c88ee9e48934bd385274b
Bartek Bugała <bartek.bugala@gmail.com>
2019-10-25 19:08:08 +0200

    add facebook comments and posts

--- client/src/components/pages/SinglePost/SinglePostPage.js
+++ client/src/components/pages/SinglePost/SinglePostPage.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import PageTitle from '../../common/PageTitle/PageTitle';
 import SinglePost from '../../features/SinglePost/SinglePostContainer';
+import { FacebookProvider, Comments, Share } from 'react-facebook';
+import { withRouter } from 'react-router-dom';
+import { BASE_URL } from '../../../config';
+import Button from '../../common/Button/Button';
 
-const SinglePostPage = props => (
-  <div>
-    <PageTitle>Single Post</PageTitle>
-    <SinglePost postId={props.match.params.id} />
-  </div>
-);
+const SinglePostPage = props => {
+  const { location } = props;
+  return (
+    <div>
+      <PageTitle>Single Post</PageTitle>
+      <FacebookProvider appId="2422007481420819">
+        <Share href={`${BASE_URL}${location.pathname}`}>
+          {({ handleClick, loading }) => (
+            <Button variant="primary" /* disabled="loading" */ onClick={handleClick}>
+              Share
+            </Button>
+          )}
+        </Share>
 
-export default SinglePostPage;
+        <SinglePost postId={props.match.params.id} />
+
+        <Comments href={`${BASE_URL}${location.pathname}`} />
+      </FacebookProvider>
+    </div>
+  );
+};
+
+export default withRouter(props => <SinglePostPage {...props} />);
