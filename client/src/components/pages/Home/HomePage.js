dcebf73bf7d8be9bee032e1c7286f2ca1de8e5aa
Bartek.B <bartek.bugala@gmail.com>
2019-10-25 12:37:02 +0200

    remove unused Counter import

--- client/src/components/pages/Home/HomePage.js
+++ client/src/components/pages/Home/HomePage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import PageTitle from '../../common/PageTitle/PageTitle';
-import PostsCounter from '../../features/PostsCounter/PostsCounterContainer';
 import Posts from '../../features/Posts/PostsContainer';
 
 const HomePage = () => (
